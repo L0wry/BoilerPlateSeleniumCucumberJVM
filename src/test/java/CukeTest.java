b119bba4aed2954a1f2fc9803685a298fa769e7d
matthew lowry <matthewlowry@MacBook-Pro-2.local>
2016-06-24 15:02:33 +0100

    0.3

--- src/test/java/CukeTest.java
+++ src/test/java/CukeTest.java
@@ -5,8 +5,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions
-        (plugin = {"pretty", "html:target/cucumber-html-report"},
-        format = {"json:target/cucumber-results.json"},
+        (plugin = {"json:target/cucumber-results.json"}, //"pretty", "html:target/cucumber-html-report",
         features = {"./src/test/resources/features"},
         glue = {"step_definitions"})
 public class CukeTest {}
